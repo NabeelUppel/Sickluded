a6a903c394745e27d5ec49cb9f621e85c502b0ea
Nabeel <nabeeluppel@gmail.com>
2020-06-28 03:50:29 +0200

    Small change

--- app/src/main/java/com/example/sickluded/SymptomsActivity.java
+++ app/src/main/java/com/example/sickluded/SymptomsActivity.java
@@ -3,12 +3,14 @@ package com.example.sickluded;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class SymptomsActivity extends AppCompatActivity {
+public class SymptomsActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_symptoms);
+        FrameLayout contentFrameLayout = findViewById(R.id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
+        getLayoutInflater().inflate(R.layout.activity_symptoms, contentFrameLayout);
     }
 }
